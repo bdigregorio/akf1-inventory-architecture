5ab59c364d61cee61b60f7a1d653d9b0bd2c6bec
Benjamin DiGregorio <ben@digregor.io>
2021-06-28 22:29:51 -0700

    Add welcome destination; wire up navigation from login

--- starter/app/src/main/java/com/udacity/shoestore/LoginFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/LoginFragment.kt
@@ -6,25 +6,36 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.databinding.FragmentLoginBinding
 
 /**
  * Fragment to handle authentication
  */
 class LoginFragment : Fragment() {
-    
+
     private lateinit var binding: FragmentLoginBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
 
+        // Set title
         activity?.title = resources.getString(R.string.login)
 
+        // bind click listeners
+        binding.loginButton.setOnClickListener(this::navigateToWelcomeScreen)
+        binding.createButton.setOnClickListener(this::navigateToWelcomeScreen)
+
         return binding.root
+    }
+
+    private fun navigateToWelcomeScreen(view: View) {
+        findNavController().navigate(R.id.action_loginFragment_to_welcomeFragment)
     }
 
     companion object {
