de24385fede81ee4724ef1a6a8af64c417edfc34
Benjamin DiGregorio <ben@digregor.io>
2021-06-29 23:45:13 -0700

    Add Instructions destination

---
+++ starter/app/src/main/java/com/udacity/shoestore/InstructionsFragment.kt
@@ -0,0 +1,27 @@
+package com.udacity.shoestore
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.udacity.shoestore.databinding.FragmentInstructionsBinding
+
+class InstructionsFragment: Fragment() {
+
+    private lateinit var binding: FragmentInstructionsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_instructions, container, false)
+
+        activity?.setTitle(R.string.instructions_title)
+
+        return binding.root
+    }
+
+}
