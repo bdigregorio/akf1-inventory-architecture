47d30fae3810e6f36289cdeaa909a1fee769215e
Benjamin DiGregorio <ben@digregor.io>
2021-07-09 17:09:01 -0700

    Add inventory destination

---
+++ starter/app/src/main/java/com/udacity/shoestore/InventoryFragment.kt
@@ -0,0 +1,42 @@
+package com.udacity.shoestore
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.udacity.shoestore.databinding.FragmentInventoryBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [InventoryFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class InventoryFragment : Fragment() {
+
+    lateinit var binding: FragmentInventoryBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_inventory, container, false)
+
+        activity?.title = resources.getString(R.string.shoe_list_title)
+
+        return binding.root
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @return A new instance of fragment ShoeListFragment.
+         */
+        @JvmStatic
+        fun newInstance() = InventoryFragment()
+    }
+}
