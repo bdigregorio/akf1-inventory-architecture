fc62ea488f68b26cc21e103e3bbc195cd7da6a7c
Benjamin DiGregorio <ben@digregor.io>
2021-07-09 17:59:47 -0700

    Link toolbar with navigation components

--- starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.udacity.shoestore
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
@@ -15,6 +18,18 @@ class MainActivity : AppCompatActivity() {
         Timber.plant(Timber.DebugTree())
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        setUpToolbar()
+    }
+
+    private fun setUpToolbar() {
         setSupportActionBar(binding.toolbar)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+        navHostFragment?.let {
+            val navController = it.findNavController()
+            val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+            binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+        }
     }
 }
