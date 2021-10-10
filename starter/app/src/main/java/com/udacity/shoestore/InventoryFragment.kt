959e203f495517b781b9f537b50c25351d307664
Benjamin DiGregorio <ben@digregor.io>
2021-10-09 17:18:01 -0700

    Update shoe list item layout to use data binding

--- starter/app/src/main/java/com/udacity/shoestore/InventoryFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/InventoryFragment.kt
@@ -4,12 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.databinding.FragmentInventoryBinding
+import com.udacity.shoestore.databinding.ItemShoeBinding
 import timber.log.Timber
 
 /**
@@ -23,11 +22,12 @@ class InventoryFragment : Fragment() {
     private val mainViewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_inventory, container, false)
+        binding = FragmentInventoryBinding.inflate(inflater, container, false)
 
         // Set title
         activity?.title = resources.getString(R.string.shoe_list_title)
@@ -63,13 +63,17 @@ class InventoryFragment : Fragment() {
                 Timber.d("Update to shoe list observed - updating UI; container initially has ${binding.shoeListContainer.childCount} views")
                 binding.shoeListContainer.removeAllViews()
                 shoes.forEach { shoe ->
-                    val newShoeView = LayoutInflater.from(context).inflate(R.layout.item_shoe, binding.shoeListContainer, false)
-                    newShoeView.findViewById<TextView>(R.id.shoe_company).text = shoe.company
-                    newShoeView.findViewById<TextView>(R.id.shoe_name).text = shoe.name
-                    newShoeView.findViewById<TextView>(R.id.shoe_size).text = getString(R.string.size_format, shoe.size)
-                    newShoeView.findViewById<TextView>(R.id.shoe_description).text = shoe.description
+                    val newShoeBinding = ItemShoeBinding.inflate(
+                        LayoutInflater.from(context),
+                        binding.shoeListContainer,
+                        false
+                    )
+                    newShoeBinding.shoeCompany.text = shoe.company
+                    newShoeBinding.shoeName.text = shoe.name
+                    newShoeBinding.shoeSize.text = getString(R.string.size_format, shoe.size)
+                    newShoeBinding.shoeDescription.text = shoe.description
                     Timber.d("Inflated view added; container now has ${binding.shoeListContainer.childCount} views")
-                    binding.shoeListContainer.addView(newShoeView)
+                    binding.shoeListContainer.addView(newShoeBinding.root)
                 }
             }
         }
